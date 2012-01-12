5f0fee5abe2bd155765f97f3a9a2a70baa0b3ff8
Kristoffer Carlsson <kcarlsson89@gmail.com>
2012-01-12 15:47:58 +0100

    added licence headers

--- src/physical_systems/anharm_osc.py
+++ src/physical_systems/anharm_osc.py
@@ -1,10 +1,25 @@
+# This file is part of FeynSimul.
+#
+# FeynSimul is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# FeynSimul is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with FeynSimul.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 Anharmonic oscillator
 """
 
 class AnHarmOsc:
 
-    def __init__(self, k = 1.0, lambd = 1.0):
+    def __init__(self, lambd = 1.0):
         """Initiates an anharmonic harmonic oscillator system in 1D.
 
         Contains the potential and virial energy operator for an anharmonic oscillator 
@@ -21,6 +36,3 @@ class AnHarmOsc:
         self.userCode = ""
         self.DOF = 1
 
-#test = AnHarmOsc(1.0);
-#print test.potential
-#print test.energyOp
