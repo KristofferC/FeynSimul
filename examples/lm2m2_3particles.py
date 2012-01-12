4dd12a332093db80c4d3effee87cd7fab50fbdb1
Kristoffer Carlsson <kcarlsson89@gmail.com>
2012-01-12 16:10:51 +0100

    fixed header

--- examples/lm2m2_3particles.py
+++ examples/lm2m2_3particles.py
@@ -11,7 +11,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Feynman.  If not, see <http://www.gnu.org/licenses/>.
+# along with FeynSimul.  If not, see <http://www.gnu.org/licenses/>.
 
 
 # Does a run with the lm2m2 potential for 3 particles.
