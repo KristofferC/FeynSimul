d02e32c0ec3c9546503c609d09735a6116d77f44
Kristoffer Carlsson <kcarlsson89@gmail.com>
2012-01-13 08:58:23 +0100

    Added plot data file functionality to pimc_kernel

--- src/pimc_utils.py
+++ src/pimc_utils.py
@@ -17,13 +17,19 @@
 import os
 from datetime import datetime
 from time import time
-import csv
+import sys
 
+import matplotlib.pyplot as plt
+import csv
 import numpy as np
 
-from host import *
+sys.path.append(sys.path[0] + "/physical_systems/")
+from lm2m2_4part import *
+sys = Lm2m2_4part()
 
-def modN(RP, savePathsInterval, systemClass, endTime
+#from host import *
+
+"""def modN(RP, savePathsInterval, systemClass, endTime
         , experimentName, opRunsFormula, mStepsPerOPRun
         , runsPerN, continueRun=False):
 
@@ -148,7 +154,7 @@ def modN(RP, savePathsInterval, systemClass, endTime
             RP.S = max(1, RP.S - 1)
 
 
-def output(filename, N, t, pathChanges, acceptanceRate
+#def output(filename, N, t, pathChanges, acceptanceRate
            , operatorMean, beta, S):
 
     print("N: " + str(N) + "\tS: " + str(S) + "\tbeta: " +
@@ -176,4 +182,107 @@ def output(filename, N, t, pathChanges, acceptanceRate
             f_data.write(str(op)+"\t")
     f_data.write("\n")
     f_data.close()
+"""
+
+def plotit(systemClass, dataFile, burnRatio, nOperators):
+
+    # Physical constants
+    a0 = 0.52917721092e-10  # Bohr radius in meters
+    kB = 1.3806488e-23  # Boltzmanns constant in J/K
+
+    # Conversion factors from units used in physical system to more
+    # standard units.
+    en2Kelv = sys.potentialUnit / kB
+    len2BRad = sys.xUnit / a0
+
+    # Load the data file and extract the parameters and op values.
+    data = np.loadtxt(dataFile)
+    generalData = data[:, 0:6]
+    opData = data[:, 6:]
+
+    nWalkers = len(opData[0]) / nOperators
+    nRuns = len(generalData)
+
+    N = data[:, 0].astype('int')
+    S = data[:, 5].astype('int')
+    AR = data[:, 3].astype('int')
+
+    # Format the opData in a 3 dimensional array where the first index is the
+    # operator, second is the walker, and the third is the run
+    operatorMeansPerRun = np.empty((nOperators, nWalkers, nRuns))
+    for i in range(nOperators):
+        operatorMeansPerRun[i] = (np.array(opData[:, i * nWalkers:
+                                 (i + 1) * nWalkers])).transpose()
+
+    n = N[0]
+    end = 0
+    operatorMeansPerRunPerN = []
+    burntOperatorMeansPerRunPerN = []
+    operatorMeansPerN = []
+    operatorMeansStdPerN = []
+    runIndexPerN = []
+    while n <= N[-1]:
+        start = end
+        if n == N[-1]:
+            end = len(N)
+        else:
+            while N[end] == N[start]:
+                end = end + 1
+        runIndexPerN.append(range(start, end))
+        operatorMeansPerRunPerN.append(operatorMeansPerRun[:, :, start:end])
+        burntOperatorMeansPerRunPerN.append(operatorMeansPerRun[:, :, (start +
+                                   int(float(end - start) * burnRatio)):end])
+        operatorMeansPerN.append(burntOperatorMeansPerRunPerN[-1].mean(axis=2))
+        n *= 2
+
+    ####################
+    # PLOT MEAN RADIUS
+    ####################
+
+    plt.figure(1)
+    for i in range(nWalkers):
+        plt.plot(operatorMeansPerRun[2, i, :] * sys.xUnit / a0)
+
+    plt.xlabel('Run number')
+    plt.ylabel('Mean radius [Bohr radius]')
+    plt.grid(True)
+
+    ####################
+    # PLOT MEAN ENERGY
+    ####################
+
+    plt.figure(2)
+    for i in range(len(operatorMeansPerRunPerN)):
+        plt.plot(runIndexPerN[i],
+                operatorMeansPerRunPerN[i][0, :, :].mean(axis=0) * en2Kelv,
+                label=str(N[runIndexPerN[i][0]]))
+    plt.xlabel('Run number')
+    plt.ylabel('Energy, [K]')
+    plt.legend(loc=4)
+    plt.grid(True)
+
+    ####################
+    # PRINT CI FOR OPS
+    ####################
+
+    print('N= ' + str(N[-1]) + ", 95% CI:")
+
+    mean = operatorMeansPerN[-1][0, :].mean() * en2Kelv
+    std = operatorMeansPerN[-1][0, :].std() / np.sqrt(nWalkers) * en2Kelv
+    print("Mean energy: [" + "%.4f" % (mean - 1.96 * std)
+            + ',' + "%.4f" % (mean + 1.96 * std) + '] K')
+
+    derivative = 0.5 / np.sqrt(operatorMeansPerN[-1][1, :].mean())
+    mean = np.sqrt(operatorMeansPerN[-1][1, :].mean()) * len2BRad
+    std = (operatorMeansPerN[-1][1, :].std() / np.sqrt(nWalkers)
+            * derivative * len2BRad)
+    print("Root mean square radius: [" + "%.4f" % (mean - 1.96 * std)
+            + ',' + "%.4f" % (mean + 1.96 * std) + '] a0')
+
+    mean = operatorMeansPerN[-1][2, :].mean() * len2BRad
+    std = operatorMeansPerN[-1][2, :].std() / np.sqrt(nWalkers) * len2BRad
+    print("Mean radius: [" + "%.4f" % (mean - 1.96 * std)
+            + ',' + "%.4f" % (mean + 1.96 * std) + '] a0')
+
+    plt.show()
 
