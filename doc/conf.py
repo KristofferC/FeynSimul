cc29f11f945324c7b7d16f7026de2c081e735cc9
Kristoffer Carlsson <kcarlsson89@gmai.com>
2012-02-07 18:17:09 +0100

    added sphinx.ext.doctest

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,8 @@ sys.path.append(currPath + "/../src/")
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.mathjax']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage',
+'sphinx.ext.pngmath', 'sphinx.ext.mathjax', 'sphinx.ext.doctest']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
