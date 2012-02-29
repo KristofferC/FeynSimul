a7d1262f896fdf28cba13334820cd5f08b05c5f4
Kristoffer Carlsson <kcarlsson89@gmail.com>
2012-02-29 17:21:19 +0100

    made an __init__ function with arguments and some variable checking

--- FeynSimul/kernel_args.py
+++ FeynSimul/kernel_args.py
@@ -13,10 +13,110 @@
 # You should have received a copy of the GNU General Public License
 # along with FeynSimul.  If not, see <http://www.gnu.org/licenses/>.
 
+"""
+This module defines a single class, C{KernelArgs} which is used to keep track
+of parameters needed to create a Kernel.
+"""
 
 class KernelArgs:
-    """
-    This module defines a single class, C{KernelArgs} which is used to keep track
-    of parameters needed to create a Kernel.
-    """
-    pass
+
+    def __init__(self,
+                 system,
+                 nbrOfWalkers,
+                 N,
+                 S,
+                 beta,
+                 enableOperator,
+                 enableCorrelator,
+                 enableBisection,
+                 enablePathShift,
+                 enableSingleNodeMove,
+                 enableBins,
+                 nbrOfWalkersPerWorkGroup,
+                 alpha=None,
+                 enableParallelizePath=None,
+                 metroStepsPerOperatorRun=None,
+                 enableGlobalPath = None,
+                 enableGlobalOldPath = None,
+                 operators=None,
+                 operatorRuns=None,
+                 correlators=None,
+                 xMin=None,
+                 xMax=None,
+                 binResolutionPerDOF=None):
+
+        self.system = system
+        self.nbrOfWalkers = nbrOfWalkers
+        self.N = N
+        self.S = S
+        self.beta = beta
+        self.nbrOfWalkersPerWorkGroup = nbrOfWalkersPerWorkGroup
+        self.enablePathShift = enablePathShift
+
+        self.enableSingleNodeMove = enableSingleNodeMove
+        if enableSingleNodeMove:
+            if alpha == None:
+                raise NameError('alpha need to be set if enableSingleNodeMove'
+                                ' is True.')
+            self.alpha = alpha
+
+        self.enableOperator = enableOperator
+        if enableOperator:
+            if operatorRuns == None:
+                raise NameError('operatorRuns need to be set if enableOperator'
+                                ' is True.')
+            if operators == None:
+                raise NameError('operators need to be set if enableOperator'
+                                ' is True.')
+            if metroStepsPerOperatorRun == None:
+                raise NameError('metroStepsPerOperatorRun need to be set'
+                                ' if enableOperator is True.')
+
+            if not isinstance(operators, tuple):
+                raise TypeError('operators need to be a tuple of strings.')
+
+            self.operatorRuns = operatorRuns
+            self.operators = operators
+            self.metroStepsPerOperatorRun = metroStepsPerOperatorRun
+
+        self.enableCorrelator = enableCorrelator
+        if enableCorrelator:
+            if correlators == None:
+                raise NameError('correlators need to be set if enableCorrelator'
+                                ' is True.')
+            if not isinstance(correlators, tuple):
+                raise TypeError('correlators need to be a tuple of strings.')
+
+            self.correlators = correlators
+
+        self.enableBins = enableBins
+        if enableBins:
+            if xMin == None:
+                raise NameError('xMin need to be set if enableBins'
+                                ' is True.')
+            if xMax == None:
+                raise NameError('xMax need to be set if enableBins'
+                                ' is True.')
+            if binResolutionPerDOF == None:
+                raise NameError('binResolutionPerDOF need to be set if'
+                                ' enableBins is True.')
+            self.xMin = xMin
+            self.xMax = xMax
+            self.binResolutionPerDOF = binResolutionPerDOF
+
+
+        self.enableBisection = enableBisection
+        if enableBisection:
+            if enableGlobalPath == None:
+                raise NameError('enableGlobalPath need to be set if'
+                                ' enableBisection is True.')
+            if enableGlobalOldPath == None:
+                raise NameError('enableGlobalOldPath need to be set'
+                                 'if enableBisection is True.')
+            if enableParallelizePath == None:
+                raise NameError('enableParallelizePath need to be set'
+                                'if enableBisection is True')
+            self.enableParallelizePath = enableParallelizePath
+            self.enableGlobalPath = enableGlobalPath
+            self.enableGlobalOldPath = enableGlobalOldPath
+
