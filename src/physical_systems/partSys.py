02cd4f0391842219db910f924ec9296b2ab350b0
Petter Säterskog <petter.saterskog@gmail.com>
2012-07-06 22:58:11 +0200

    separated openCL code to it's own file for lm2m2.h, also doing some debugging. Commit just to move files.

--- src/physical_systems/partSys.py
+++ src/physical_systems/partSys.py
@@ -8,17 +8,18 @@ def getHyperRadialPotential(x,distFun,interactions):
         if len(part)==l:
             return interaction(hyperSqrRadius([x[p] for p in part]))
         return _sum(act(l,interaction,part+[j]) for j in
-            range(len(x)-l+len(part)+1) if part==[]or j>part[-1])
+            range(len(x)-l+len(part)+1) if part==[] or j>part[-1])
     return _sum(act(i+2,interactions[i]) for i in range(len(interactions)))
 
-''':
+
 #just some testing of this code. It should be able to take any objects as x,
 #interactions must return objects that together can be operands to +.
 import sympy
-n=4;d=2
+n=4;
+d=2
 x=[sympy.Matrix([sympy.symbols('x'+str(i)), sympy.symbols('y'+str(i))]) for i in range(n)]
 m=[sympy.symbols('m')]*n
 f=[(lambda shr,i=i:sympy.symbols('f'+str(i))(shr)) for i in range(n-1)]
 print(f)
 sympy.pprint( getHyperRadialPotential(x,lambda
-    a,b:((a-b).transpose()*(a-b))[0],f).simplify()  )'''
+    a,b:((a-b).transpose()*(a-b))[0],f).simplify())
