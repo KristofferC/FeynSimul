d91ec2aa7b6ffe1dc0e8e52d7d642a2f216ec8c6
Patric Holmvall <patric.hol@gmail.com>
2012-07-08 23:06:13 +0200

    extended double support in lm2m2_4part

--- FeynSimul/GPUsrc/kernel.c
+++ FeynSimul/GPUsrc/kernel.c
@@ -42,6 +42,13 @@
     
     // Check that pragmas for 64bit actually exists
     #pragma OPENCL EXTENSION cl_khr_fp64 : enable
+    //#ifdef cl_khr_fp64
+    //    #pragma OPENCL EXTENSION cl_khr_fp64 : enable
+    //#elif defined(cl_amd_fp64)
+    //    #pragma OPENCL EXTENSION cl_amd_fp64 : enable
+    //#else
+    //    #error "Double precision floating point not supported by OpenCL implementation."
+    //#endif
 #else
     // Set float precision to single
     #define FLOAT_TYPE float
