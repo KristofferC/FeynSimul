6098bdae7c4bee08e6e425bb271e7eea553ae99b
Patric Holmvall <patric.hol@gmail.com>
2012-07-19 22:53:48 +0200

    benchmark plot script

--- benchmark_xorshift_vs_ranlux.py
+++ benchmark_xorshift_vs_ranlux.py
@@ -4,37 +4,30 @@ from matplotlib import rc
 from matplotlib.ticker import *
 import matplotlib.pyplot as plt
 
-xor = np.array([0.000835, 0.021259, 0.077566, 2.113641],
-               [0.000833, 0.021227, 0.077535, 2.116926]) # xorshift
+xor = np.array([[0.000835, 0.021259, 0.077566, 2.113641],
+                [0.000833, 0.021227, 0.077535, 2.116926]]) # xorshift
 
-r0 = np.array([0.002772, 0.013913, 0.124960, 1.234409],
-              [0.003888, 0.024675, 0.233399, 2.315815])  # ranlux0
+ranlux = np.array([[0.002772, 0.013913, 0.124960, 1.234409],   #ranlux0 single
+                   [0.003888, 0.024675, 0.233399, 2.315815],   #ranlux0 double
+               
+                   [0.003372, 0.019870, 0.184641, 1.833603],   #ranlux1 single
+                   [0.005065, 0.036373, 0.349675, 3.486245],   #ranlux1 double
+               
+                   [0.005217, 0.037840, 0.364948, 3.635847],   #ranlux2 single
+                   [0.008705, 0.072788, 0.715689, 7.143060],   #ranlux2 double
+               
+                   [0.008220, 0.068618, 0.674307, 6.730529],   #ranlux3 single
+                   [0.014878, 0.134723, 1.334791, 13.330784],  #ranlux3 double
+               
+                   [0.012606, 0.113489, 1.124352, 11.224786],  #ranlux4 single
+                   [0.023882, 0.225516, 2.243392, 22.421897]]) #ranlux4 double
 
-r1 = np.array([0.003372, 0.019870, 0.184641, 1.833603],
-              [0.005065, 0.036373, 0.349675, 3.486245])  # ranlux1
-
-r2 = np.array([0.005217, 0.037840, 0.364948, 3.635847],
-              [0.008705, 0.072788, 0.715689, 7.143060])  # ranlux2
-
-r3 = np.array([0.008220, 0.068618, 0.674307, 6.730529]),
-              [0.014878, 0.134723, 1.334791, 13.330784)] # ranlux3
-
-r4 = np.array([0.012606, 0.113489, 1.124352, 11.224786],
-              [0.023882, 0.225516, 2.243392, 22.421897]) # ranlux4
+nbrOfThreads = 448*32*4;
+randsteps = np.array([1000,10000,100000,1000000])
 
 logplot = True   # Log the x axis or not
 
 msize = 40
-xs_label = 'xorshift'
-r0_label = 'RANLUX, lux=0'
-r1_label = 'RANLUX, lux=1'
-r2_label = 'RANLUX, lux=2'
-r3_label = 'RANLUX, lux=3'
-r4_label = 'RANLUX, lux=4'
-
-single_label = ' , 32bit'
-double_label = ' , 64bit'
-
 fig_width_pt = 512.0
 legendloc = 4
 fsize=16
@@ -63,34 +56,42 @@ pl.figure(1)
 if logplot:
     pl.semilogx()
 
-# Dict with settings for different platforms
-gpus = {'xorshift': {'Label': gtx470label,'Show': True, 'Marker': '^',
-                   'MarkerSize': msize, 'Color': 'b', 'norm': None,
-                   'cmap': None, 'vmin': None, 'vmax': None, 'Alpha': 1.0,
-                   'LineWidths': 0.1, 'Verts': None},
-        'ranlux0': {'Label': gtx560label, 'Show': False, 'Marker': '>',
-                     'MarkerSize': msize, 'Color': 'tomato', 'norm': None,
-                     'cmap': None, 'vmin': None, 'vmax': None, 'Alpha': 0.5,
-                     'LineWidths': 0.1, 'Verts': None},
-        'ranlux1': {'Label': hd5850label, 'Show': True, 'Marker': '<',
-                   'MarkerSize': msize, 'Color': 'g', 'norm': None,
-                   'cmap': None, 'vmin': None, 'vmax': None, 'Alpha': 0.5,
-                   'LineWidths': 0.1, 'Verts': None},
-        'ranlux2': {'Label': hd5850label, 'Show': True, 'Marker': '<',
-                   'MarkerSize': msize, 'Color': 'g', 'norm': None,
-                   'cmap': None, 'vmin': None, 'vmax': None, 'Alpha': 0.5,
-                   'LineWidths': 0.1, 'Verts': None},
-        'ranlux3': {'Label': hd5850label, 'Show': True, 'Marker': '<',
-                   'MarkerSize': msize, 'Color': 'g', 'norm': None,
-                   'cmap': None, 'vmin': None, 'vmax': None, 'Alpha': 0.5,
-                   'LineWidths': 0.1, 'Verts': None},
-        'ranlux4': {'Label': gt9600label, 'Show': False, 'Marker': 'o',
-                   'MarkerSize': msize, 'Color': 'g', 'norm': None,
-                   'cmap': None, 'vmin': None, 'vmax': None, 'Alpha': 0.5,
-                   'LineWidths': 0.1, 'Verts': None}}
+# xorshift plot
+pl.plot(randsteps,
+        xor[1],'r-',
+        linewidth=3.0,
+        label=('xorshift, 32bit'))
+
+linestyle = ('g-','b-','c-','k-','m-')
+markerstyle = ('','','','','')
+for i in range(0,5):
+    pl.plot(randsteps,
+            ranlux[i*2],linestyle[i],
+            linewidth=2.0,
+            label=('RANLUX, lux='+str(i)+', 32bit'),
+            markersize=mszie,
+            marker=markerstyle(i))
+linestyle = ('g--','b--','c--','k--','m--')
+markerstyle = ('','','','','')
+for i in range(0,5):
+    pl.plot(randsteps,
+            ranlux[i*2+1],'r--',
+            linewidth=2.0,
+            label=('RANLUX, lux='+str(i)+', 64bit'),
+            markersize=mszie,
+            marker=markerstyle(i))
+
+pl.title(r'xorshift vs RANLUX on 448*32*4 (=57344) threads')
+pl.xlabel(r'Number of randoms per thread')
+pl.ylabel(r'Time [sec]')
+
+#        'ranlux4': {'Label': gt9600label, 'Show': False, 'Marker': 'o',
+#                   'MarkerSize': msize, 'Color': 'g', 'norm': None,
+#                   'cmap': None, 'vmin': None, 'vmax': None, 'Alpha': 0.5,
+#                   'LineWidths': 0.1, 'Verts': None}}
 
 
-
+"""
 gmax = 0.0
 for gpu in gpus:
     print gpus[gpu]['Label'] + ': ' + str(gpus[gpu]['Show'])
@@ -156,7 +157,7 @@ pl.xticks(np.arange(np.ceil(xr).astype(np.uint32)+1)*10**4,
 plt.text(float(xr + 0.1)*10000.0,
          2*10.0**7,r'$\times10^4$',
          fontsize=0.8*fsize)
-
+"""
 # Draw legend
 pl.legend(loc=legendloc)
 
